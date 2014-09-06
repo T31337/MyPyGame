df1a82ada3b27f65e6d6891a9c5e561d8dd03734
T31337 <TL1337@yahoo.com>
2014-09-06 12:15:07 -0500

    :)

--- MySprite.py
+++ MySprite.py
@@ -1,23 +1,25 @@
 import pygame
 from colors import *
-from pygame.locals import *
+import pygame.sprite.Sprite
+
 class MySprite(pygame.sprite.Sprite):
-    def __init__(self,color=blue,width=64,height=64):
+    def __init__(self, color=blue, width=64, height=64):
         pygame.sprite.Sprite.__init__(self)
         
         self.image=pygame.surface.Surface((width,height))
         self.image.fill(color)
         self.setProperties()
-    def setPos(self,x,y):
-        self.rect.x = self.rect.centerx
-        self.rect.y = self.rect.centery
+
+    def setPos(self, x, y):    
+        self.rect.centerx = x
+        self.rect.centery = y
         
     def setImage(self,fileName=None):
         if fileName != None:
             self.image = pygame.image.load(fileName)
-            self.setProperties()          
+            #self.setProperties()          
             
     def setProperties(self):
         self.rect = self.image.get_rect()
-        self.origin_x = self.rect.centerx
-        self.origin_y = self.rect.centery        
+        #self.origin_x = self.rect.centerx
+        #self.origin_y = self.rect.centery  
